6ed6e46f785d4d23835ac6467b2f7c5d3f8cf558
Mihir <mihir.dholakia@somaiya.edu>
2022-01-27 02:03:27 +0530

    about us

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Eterna - v4.7.0
-* Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
